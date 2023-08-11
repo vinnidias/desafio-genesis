1f0023d699b6cb1bf68605b9757008d38b552120
vinnidias <vinicius.3991@gmail.com>
2023-08-11 19:13:00 -0300

    abstracting components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,21 @@
 "use client";
 
-import { BookCard } from "@/components/BookCard";
-import { api } from "@/services/api";
-import { useEffect, useState } from "react";
+import { BookList } from "@/components/BookList";
+import { SearchInput } from "@/components/SearchInput";
+import { useBooks } from "@/hooks/useBooks";
 
 export default function Home() {
-  const [booksList, setBooksList] = useState([]);
+  const { inputSearch, setInputSearch, handleSearch } = useBooks();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await api.get(
-          "volumes?q=nextjs&key=AIzaSyCv6uOgcltMLEgv9LaRqrbVY4HHVRzgVm8"
-        );
-        console.log("success: ", res.data);
-        setBooksList(res.data.items);
-      } catch (error) {
-        console.log("fail: ", error);
-      }
-    })();
-  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-start p-2 gap-2">
-      <h1>Home</h1>
-      <div className="grid grid-cols-1 grid-rows-2 gap-2 gap-y-6 md:grid-cols-4 lg:grid-cols-5  ">
-        {booksList.length > 0 &&
-          booksList.map((book: any, index) => {
-            const imgLink = book.volumeInfo.imageLinks;
-            console.log("link da img: ", imgLink);
-            return (
-              <BookCard
-                imgLink={imgLink}
-                title={book.volumeInfo.title}
-                authors={book.volumeInfo.authors}
-                pageLink="/description"
-                key={index}
-              />
-            );
-          })}
-      </div>
+    <main className="flex min-h-screen flex-col items-center p-2 px-2 gap-6 md:px-20">
+      <h1 className="pt-6 text-lg font-semibold">Busca Livro</h1>
+      <SearchInput
+        value={inputSearch}
+        onChange={(e) => setInputSearch(e.target.value)}
+        handleClick={handleSearch}
+      />
+      <BookList />
     </main>
   );
 }
