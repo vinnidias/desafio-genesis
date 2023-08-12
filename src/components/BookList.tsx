ad5f9cd975d77785537a5ad6df4385c2d4f8ab6f
vinnidias <vinicius.3991@gmail.com>
2023-08-12 19:23:45 -0300

    passing route params and remove any type

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -9,14 +9,14 @@ export const BookList = ({ booksList }: IProps) => {
   return (
     <div className="grid grid-cols-1 grid-rows-2 gap-2 gap-y-6 w-full md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-col-8">
       {booksList.length > 0 &&
-        booksList.map((book: any, index) => {
+        booksList.map((book, index) => {
           const imgLink = book.volumeInfo.imageLinks;
           return (
             <BookCard
               imgLink={imgLink}
               title={book.volumeInfo.title}
               authors={book.volumeInfo.authors}
-              pageLink="/description"
+              pageLink={`/description/book/${book.id}`}
               key={index}
             />
           );
