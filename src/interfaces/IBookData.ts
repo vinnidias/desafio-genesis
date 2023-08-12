7fb7013bb99dfe6fac3da8f238097e787ef5cdbb
vinnidias <vinicius.3991@gmail.com>
2023-08-12 14:30:52 -0300

    books data interface

---
+++ src/interfaces/IBookData.ts
@@ -0,0 +1,64 @@
+export interface IBookData {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher: string;
+    publishedDate: string;
+    industryIdentifiers: {
+      type: string;
+      identifier: string;
+    }[];
+    readingModes: {
+      text: boolean;
+      image: boolean;
+    };
+    pageCount: number;
+    printType: string;
+    averageRating: number;
+    ratingsCount: number;
+    maturityRating: string;
+    allowAnonLogging: boolean;
+    contentVersion: string;
+    panelizationSummary: {
+      containsEpubBubbles: boolean;
+      containsImageBubbles: boolean;
+    };
+    imageLinks?: {
+      smallThumbnail: string;
+      thumbnail: string;
+    };
+    language: string;
+    previewLink: string;
+    infoLink: string;
+    canonicalVolumeLink: string;
+  };
+  saleInfo: {
+    country: string;
+    saleability: string;
+    isEbook: boolean;
+  };
+  accessInfo: {
+    country: string;
+    viewability: string;
+    embeddable: boolean;
+    publicDomain: boolean;
+    textToSpeechPermission: string;
+    epub: {
+      isAvailable: boolean;
+    };
+    pdf: {
+      isAvailable: boolean;
+      acsTokenLink: string;
+    };
+    webReaderLink: string;
+    accessViewStatus: string;
+    quoteSharingAllowed: boolean;
+  };
+  searchInfo: {
+    textSnippet: string;
+  };
+}
