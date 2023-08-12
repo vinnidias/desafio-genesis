83cd1f875b0444c936ed8a745790cf31dbffb9f7
vinnidias <vinicius.3991@gmail.com>
2023-08-12 14:28:39 -0300

    using data with props

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,9 +1,12 @@
 import { useBooks } from "@/hooks/useBooks";
 import { BookCard } from "./BookCard";
+import { IBookData } from "@/interfaces/IBookData";
 
+interface IProps {
+  booksList: IBookData[];
+}
 
-export const BookList = () => {
-  const { booksList } = useBooks()
+export const BookList = ({ booksList }: IProps) => {
   return (
     <div className="grid grid-cols-1 grid-rows-2 gap-2 gap-y-6 w-full md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-col-8">
       {booksList.length > 0 &&
