f99474963ef3ed4d570eee2eae26ef44c566c0e1
vinnidias <vinicius.3991@gmail.com>
2023-08-12 22:44:22 -0300

    remove user handle button

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,12 +3,11 @@ import { ChangeEvent } from "react";
 interface IProps {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   value: string;
-  handleClick: () => void;
 }
 
-export const SearchInput = ({ onChange, value, handleClick }: IProps) => {
+export const SearchInput = ({ onChange, value }: IProps) => {
   return (
-    <div className="flex w-full max-w-max gap-3">
+    <div className="flex w-full max-w-[30em] gap-3">
       <input
         type="text"
         placeholder="Digite o nome, autor ou editora do livro"
@@ -16,14 +15,6 @@ export const SearchInput = ({ onChange, value, handleClick }: IProps) => {
         value={value}
         className="border-2 border-grey rounded outline-none p-1 min-w-full"
       />
-      <button
-        onClick={handleClick}
-        disabled={value ? false : true}
-        className="w-48 p-1 rounded bg-sky-300 text-white transition-all shadow-md hover:bg-sky-200"
-        style={{ cursor: value ? "pointer" : "not-allowed" }}
-      >
-        Pesquisar
-      </button>
     </div>
   );
 };
